9c20feb113f409d5dd8f5657e910634ca767b2fc
Adrien Lenoir <lenoiradrien1@gmail.com>
2021-09-28 13:56:09 +0200

    Express ejs layout

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser"
 import router from "./router/router.mjs"
 import dotenv from 'dotenv'
 import errorController from "./controllers/error.controller.mjs";
+import expressLayout from 'express-ejs-layouts'
 
 const app = express()
 dotenv.config()
@@ -10,7 +11,11 @@ dotenv.config()
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(expressLayout)
 app.use(router)
+
+app.set('layout extractScripts', true)
+app.set('layout extractStyles', true)
 
 app.get('*', errorController.error404)
 
