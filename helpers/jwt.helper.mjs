9648a0008b40275c29832f2d7c64788607f13321
Adrien Lenoir <lenoiradrien1@gmail.com>
2021-09-28 15:54:45 +0200

    helpers

---
+++ helpers/jwt.helper.mjs
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken")
+const JWT_SIGN_SECRET = process.env.JWT_SIGN_SECRET
+
+export default {
+  generate: (userId) => {
+    return jwt.sign(
+      { id: userId },
+      JWT_SIGN_SECRET,
+      { expiresIn: "24h", }
+    )
+  },
+  verify: (token) => {
+    if (token != null) {
+      try {
+        const jwtToken = jwt.verify(token, JWT_SIGN_SECRET)
+
+        return jwtToken != null && jwtToken.id != null
+      } catch (error) {}
+    }
+
+    return false
+  }
+}
