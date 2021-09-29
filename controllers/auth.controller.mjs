dc1dd5297888d136f7f1443bc02976ab1c8e16f8
MathisVkg <mvk181@gmail.com>
2021-09-29 10:19:05 +0200

    edit

--- controllers/auth.controller.mjs
+++ controllers/auth.controller.mjs
@@ -9,5 +9,15 @@ export default {
       success: false,
       message: "formulaire invalide"
     })
+  },
+  signupShow: async (req, res) => {
+    res.render('auth/signup', { test: "lol" })
+  },
+  signupAction: async (req, res) => {
+    console.log(req.body)
+    return res.json({
+      success: false,
+      message: "formulaire invalide"
+    })
   }
 }
